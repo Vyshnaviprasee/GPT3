008745279df1f764759759ab1bbec8f4e659d7c7
Vyshnaviprasee <vyshnaviprasee@gmail.com>
2024-06-27 21:59:19 +0530

    first commit

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Feature from '../../compoents/feature/Feature'
+
+import './whatGPT3.css'
+
+const WhatGPT3 = () => {
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="What is GPT-3"  text="we so opinion friends me message as delight. Whole front do of plate heard oh. His explain toiled as delight. Whole front do of plate heard oh."/>
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title={"Chatbots"} text={"We so opinion friends me message as delight. Whole front do of plate heard oh. His explain toiled as delight. Whole front do of plate heard oh."}/>
+        <Feature title={"Knowledgebase"} text={"We so opinion friends me message as delight. Whole front do of plate heard oh. His explain toiled as delight. Whole front do of plate heard oh."} />
+        <Feature title={"Education"} text={"We so opinion friends me message as delight. Whole front do of plate heard oh. His explain toiled as delight. Whole front do of plate heard oh."} />
+       
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3
